fa03362e628e56279f46fcd7f62e18d914257fe0
Marco Montilla <marco.montilla@telrite.com>
2024-11-11 09:21:26 -0500

    Sanity Schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
   src: [
-    { path: "./fonts/WorkSans-Black.ttf",      weight: "900", style: "normal" },
-    { path: "./fonts/WorkSans-ExtraBold.ttf",  weight: "800", style: "normal" },
-    { path: "./fonts/WorkSans-Bold.ttf",       weight: "700", style: "normal" },
-    { path: "./fonts/WorkSans-SemiBold.ttf",   weight: "600", style: "normal" },
-    { path: "./fonts/WorkSans-Medium.ttf",     weight: "500", style: "normal" },
-    { path: "./fonts/WorkSans-Regular.ttf",    weight: "400", style: "normal" },
-    { path: "./fonts/WorkSans-Thin.ttf",       weight: "300", style: "normal" },
-    { path: "./fonts/WorkSans-Light.ttf",      weight: "200", style: "normal" },
+    { path: "./fonts/WorkSans-Black.ttf", weight: "900", style: "normal" },
+    { path: "./fonts/WorkSans-ExtraBold.ttf", weight: "800", style: "normal" },
+    { path: "./fonts/WorkSans-Bold.ttf", weight: "700", style: "normal" },
+    { path: "./fonts/WorkSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "./fonts/WorkSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "./fonts/WorkSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "./fonts/WorkSans-Thin.ttf", weight: "300", style: "normal" },
+    { path: "./fonts/WorkSans-Light.ttf", weight: "200", style: "normal" },
     { path: "./fonts/WorkSans-ExtraLight.ttf", weight: "200", style: "normal" },
   ],
-  variable: '--font-work-sans'
+  variable: "--font-work-sans",
 });
 
 export const metadata: Metadata = {
@@ -29,11 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={workSans.variable}
-      >
-        {children}
-      </body>
+      <body className={workSans.variable}>{children}</body>
     </html>
   );
 }
